e358230899bfbd9b11f5cbe3e4adb357394f5368
Nikhilesh <nikhilesh.meshram@gmail.com>
2016-05-25 00:43:02 +0530

    Created a boxx

--- main.cpp
+++ main.cpp
@@ -23,9 +23,10 @@ class ControllerC
 	ControllerC(){mode_m = MainMenu;};
 	//Game State
 	GMode mode_m;
-	void init();
 	public:
-	ControllerC* getInstance();
+	void init();
+	void clear();
+	static ControllerC* getInstance();
 };
 
 ControllerC* gcntrl = NULL;
@@ -37,50 +38,45 @@ ControllerC* ControllerC::getInstance()
 	return gcntrl;	
 }
 
+void ControllerC::init()
+{
+	initscr();	//Intializes Ncurses module
+	cbreak();	//Disable line bufferring
+	keypad(stdscr,TRUE);	//Allows special function keys
+}
+	
+void ControllerC::clear()
+{
+	endwin();
+}
+
+
+class Renderer
+{
+};
+
 int main()
 {
-	std::cout<<"lets get started"<<std::endl;
-	
-	// SCREEN SIZE 80x80
-	char pixels[SCREEN_HEIGHT][SCREEN_WIDTH/8];
-	memset(pixels,0,SCREEN_HEIGHT*SCREEN_WIDTH/8*sizeof(char));
-	// A(5,6)
-	int start_x=0;
-	int start_y=0;
+	(ControllerC::getInstance())->init();
+	WINDOW* myWin;
+	int height = 25;
+	int width = 50;
+	int start_y = (LINES-height)/2;
+	int start_x = (COLS-width)/2;
+	printw("Press F1 to exit");
+	refresh();
+	myWin = newwin(height,width,start_y,start_x);
+	box(myWin,0,0);	//Draw Box around window
+	wrefresh(myWin);
 
-	// B(10,25)
-	int end_x=1;
-	int end_y=79;
-	
-	for (int y=0;y<SCREEN_HEIGHT;y++)
+	int ch;
+	while((ch = getch()) != KEY_F(1))
 	{
-		for(int x=0;x<SCREEN_WIDTH;x++)
-		{
-			//condition to set (x,y)
-			// equation of line y=mx+c
-			double m=(end_y-start_y)/(end_x-start_x);
-			double c=((start_y*(end_x-start_x))-(start_x*(end_y-start_y)))/(end_x-start_x);
-			double proximity=std::abs((y*(end_x-start_x)-x*(end_y-start_y)-start_y*end_x+start_x*end_y)/(end_x-start_x)); 
-			//std::cout<<std::abs(6-m*5-c)<<" "<<std::abs(25-m*10-c)<<std::endl;
-			if ((x>=start_x && x<=end_x) && (y>=start_y && y<=end_y) && (proximity<=0.707))
-			{
-				std::cout<<"("<<x<<","<<y<<")"<<std::endl;
-				pixels[y][x/8]|=(1<<(7-(x%8)));
-			}
-		}
 	}
 	
-	for (int y=SCREEN_HEIGHT-1;y>-1;y--)
-	{
-		for(int x=0;x<SCREEN_WIDTH;x++)
-		{
-			if (pixels[y][x/8]&(1<<(7-(x%8))))
-				std::cout<<" 1";
-			else
-				std::cout<<" 0";
-		}
-		std::cout<<std::endl;
-	}
-
+	delwin(myWin);
+	endwin();
+	
+//		(ControllerC::getInstance())->clear();
 	return 0;
 }
